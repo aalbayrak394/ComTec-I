f10949a6cdd0994d168a0f8715cf443eb07b75d4
Aleyna Albayrak <a.albayrak@uni-kassel.de>
2024-07-13 14:32:18 +0200

    feat: fisher score feature selection

--- dataset/cycling_dataset.py
+++ dataset/cycling_dataset.py
@@ -3,22 +3,28 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-class CyclingDataset(Dataset):
-    def __init__(self, file_path_acc, file_path_gyro):
-        with h5py.File(file_path_acc, 'r') as hf:
+def get_dataset(file_path_acc, file_path_gyro):
+    with h5py.File(file_path_acc, 'r') as hf:
             data_acc = hf['windows'][:]
             labels_acc = hf['labels'][:]
 
-        with h5py.File(file_path_gyro, 'r') as hf:
-            data_gyro = hf['windows'][:]
-            labels_gyro = hf['labels'][:]
-        
-        assert len(data_acc) == len(data_gyro)
-        assert len(labels_acc) == len(labels_gyro)
-        assert np.all(labels_acc == labels_gyro)
+    with h5py.File(file_path_gyro, 'r') as hf:
+        data_gyro = hf['windows'][:]
+        labels_gyro = hf['labels'][:]
+    
+    assert len(data_acc) == len(data_gyro)
+    assert len(labels_acc) == len(labels_gyro)
+    assert np.all(labels_acc == labels_gyro)
 
-        self.data = np.concatenate([data_acc, data_gyro], axis=-1)
-        self.labels = labels_acc
+    data = np.concatenate([data_acc, data_gyro], axis=-1)
+    labels = labels_acc
+
+    return data, labels
+
+
+class TorchCyclingDataset(Dataset):
+    def __init__(self, file_path_acc, file_path_gyro):
+        self.data, self.labels = get_dataset(file_path_acc, file_path_gyro)
 
     def __len__(self):
         return len(self.data)
---
+++ scripts/evaluate.py
@@ -0,0 +1,6 @@
+""" Evaluate different feature selection algorithms with RandomForestClssifier """
+
+# TODO: read segmented data
+# TODO: extract features
+# TODO: apply different feature selection algorithms
+# TODO: evaluate each subset of features with RandomForestClassifier
